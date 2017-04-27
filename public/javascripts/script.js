93d8fbc600ad50167034f6942f2beee22f55658d
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-04-27 16:46:34 -0300

    fazendo modal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,7 +5,7 @@ function tudo(){
 	$('#tabela').empty(); //Limpando a tabela
     $.get(server, function(data) {
         for (i in data){
-        	$('#tabela').append('<tr><td class="tamanho"><img src='+data[i].imagem+'></td><td>'+data[i].nome+'</td><td>'+data[i].categoria+'</td><td>'+data[i].estoque+'</td><td>'+data[i].material+'</td><td>'+data[i].tamanho+'</td><td>'+data[i].cor+'</td><td>'+data[i].preco+'</td></tr>')
+        	$('#tabela').append('<tr><td class="tamanho"><img src='+data[i].imagem+'></td><td>'+data[i].nome+'</td><td>'+data[i].categoria+'</td><td>'+data[i].estoque+'</td><td>'+data[i].material+'</td><td>'+data[i].tamanho+'</td><td>'+data[i].cor+'</td><td>'+data[i].preco+'</td><td><a class="waves-effect waves-light btn"><i class="material-icons ">mode_edit</i></a></td><td><a class="waves-effect waves-light btn red"><i class="material-icons ">delete</i></a></td></tr>')
         }
     });
 
@@ -15,4 +15,5 @@ function tudo(){
 $(document).ready(function(){
         tudo();
         $('.modal').modal();
+        $('select').material_select();
 });
