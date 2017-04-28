bb3e2be449c180077a65ea8b6ad3e2f576984455
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-04-28 17:53:40 -0300

    alteraçoes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,9 +11,15 @@ function tudo(){
 
 }
 
+function verificaNumeroEstoque(e) {
+    if ((e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) || e.which ==32 )   {
+         return false;
+    }
+}
 
 $(document).ready(function(){
         tudo();
         $('.modal').modal();
         $('select').material_select();
+        $('#estoque').keypress(verificaNumeroEstoque);
 });
